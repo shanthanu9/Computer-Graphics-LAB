d2e4a5c21f2e3053c7050c4e3fe38d9e774ad1b3
shanthanu9 <shanthanu.s.rai9@gmail.com>
2020-01-15 14:13:12 +0530

    Add DDA line drawing algorithm

---
+++ lab3/dda.cpp
@@ -0,0 +1,71 @@
+#include <GL/glut.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define abs(a) ((a<0)?(-a):(a))
+#define sign(a) ((a>0)?1:-1)    // NOTE: sign(0) = 0
+
+int x1, x2;
+int y1, y2;
+
+void initGL() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity();
+    gluOrtho2D( 0.0, 500.0, 0.0, 500.0 );
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+    float xi, yi;
+    int steps;
+    if(abs(dx) > abs(dy)) {
+        steps = abs(dx);
+        xi = 1 * sign(dx);
+        yi = (float)dy/dx * sign(dy);
+    }
+    else {
+        steps = abs(dy);
+        xi = (float)dx/dy * sign(dx);
+        yi = 1 * sign(dy);
+    }
+
+    glBegin(GL_POINTS);
+        glColor3f(1,1,1);
+        float x = x1, y = y1;
+        for(int i = 0; i <= steps; i++) {
+            glVertex2i(x, y);
+            x += xi;
+            y += yi;
+        }
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(80, 80);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("DDA Line drawing");
+
+    if(argc == 5) {
+        x1 = atoi(argv[1]);
+        y1 = atoi(argv[2]);
+        x2 = atoi(argv[3]);
+        y2 = atoi(argv[4]);
+    }
+    else {
+        printf("usage: ./a.out x1 y1 x2 y2\n");
+        return 1;
+    }
+
+    glutDisplayFunc(display);
+    initGL();
+    glutMainLoop();
+    return 0;
+}
