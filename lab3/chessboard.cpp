a5da0e717b30f50a03533e699a8ea19ba715f888
shanthanu9 <shanthanu.s.rai9@gmail.com>
2020-01-15 15:06:41 +0530

    Add chessboard

---
+++ lab3/chessboard.cpp
@@ -0,0 +1,68 @@
+#include <GL/glut.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define abs(a) ((a<0)?(-a):(a))
+#define sign(a) ((a>0)?1:-1)    // NOTE: sign(0) = 0
+
+int x1, x2;
+int y1, y2;
+
+void initGL() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity();
+    gluOrtho2D( 0.0, 500.0, 0.0, 500.0 );
+}
+
+void drawLine(int x1, int y1, int x2, int y2) {
+    
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+    float xi, yi;
+    int steps;
+    if(abs(dx) > abs(dy)) {
+        steps = abs(dx);
+        xi = 1 * sign(dx);
+        yi = (float)dy/dx * sign(dy);
+    }
+    else {
+        steps = abs(dy);
+        xi = (float)dx/dy * sign(dx);
+        yi = 1 * sign(dy);
+    }
+
+    glBegin(GL_POINTS);
+        glColor3f(1,1,1);
+        float x = x1, y = y1;
+        for(int i = 0; i <= steps; i++) {
+            glVertex2i(x, y);
+            x += xi;
+            y += yi;
+        }
+    glEnd();
+}
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    for(int i = 0; i < 8; i++) {
+        drawLine(5, 5+70*i, 495, 5+70*i);
+        drawLine(5+70*i, 5, 5+70*i, 495);
+    }
+
+    glFlush();
+}
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(80, 80);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("DDA Line drawing");
+
+    glutDisplayFunc(display);
+    initGL();
+    glutMainLoop();
+    return 0;
+}
